c8787231035b4f01615578627ee7422d0c0a1e73
aminhl <amin.hlel@esprit.tn>
2023-07-08 13:25:35 +0100

    create_JwtService

--- src/main/java/com/aminhl/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/aminhl/security/config/JwtAuthenticationFilter.java
@@ -14,6 +14,8 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
@@ -21,10 +23,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwtToken;
+        final String userEmail;
         if (authHeader == null || !authHeader.startsWith("Bearer ")){
             filterChain.doFilter(request, response);
             return;
         }
         jwtToken = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwtToken);
     }
 }
---
+++ src/main/java/com/aminhl/security/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.aminhl.security.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String jwtToken) {
+        return null;
+    }
+}
