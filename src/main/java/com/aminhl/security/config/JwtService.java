8747c93bed36686b7950778e3a8c634fec4ece64
aminhl <amin.hlel@esprit.tn>
2023-07-08 13:46:09 +0100

    extract_claims_from_jwt

--- src/main/java/com/aminhl/security/config/JwtService.java
+++ src/main/java/com/aminhl/security/config/JwtService.java
@@ -1,11 +1,22 @@
 package com.aminhl.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
-    public String extractUsername(String jwtToken) {
+    public String extractUsername(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigninKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
