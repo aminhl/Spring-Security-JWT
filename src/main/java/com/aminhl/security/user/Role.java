dafaac72a6f1ea402de080401591dc5a25780f65
aminhl <amin.hlel@esprit.tn>
2023-07-08 12:02:38 +0100

    implimenting_UserDetails

---
+++ src/main/java/com/aminhl/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.aminhl.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/aminhl/security/user/User.java
+++ src/main/java/com/aminhl/security/user/User.java
@@ -2,6 +2,12 @@ package com.aminhl.security.user;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -9,7 +15,7 @@ import lombok.*;
 @AllArgsConstructor
 @Entity
 @Table(name = "_user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Integer id;
@@ -17,5 +23,41 @@ public class User {
     private String lastname;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
