2f2e71ce4ba379f8e27d4d9fc753f0d85d2b3445
aminhl <amin.hlel@esprit.tn>
2023-07-10 15:07:14 +0100

    impliment_getSigninKey

--- src/main/java/com/aminhl/security/config/JwtService.java
+++ src/main/java/com/aminhl/security/config/JwtService.java
@@ -2,10 +2,19 @@ package com.aminhl.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.*;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
 
     public String extractUsername(String token) {
         return null;
@@ -18,5 +27,10 @@ public class JwtService {
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    private Key getSigninKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
