e94a2389fc1e3a64c1ede2da8664f2f2ea160290
aminhl <amin.hlel@esprit.tn>
2023-07-16 16:30:15 +0100

    refactor: adding_filterchain

--- src/main/java/com/aminhl/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/aminhl/security/config/JwtAuthenticationFilter.java
@@ -49,5 +49,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
         }
+        filterChain.doFilter(request, response);
     }
 }
