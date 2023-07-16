373169b5c86a16ae8c317e5d5bd3ea8651255759
aminhl <amin.hlel@esprit.tn>
2023-07-16 14:07:56 +0100

    update_the_securitycontextholder_and_finish_the_filter

--- src/main/java/com/aminhl/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/aminhl/security/config/JwtAuthenticationFilter.java
@@ -39,6 +39,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userEmail = jwtService.extractUsername(jwtToken);
         if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if (jwtService.isTokenValid(jwtToken, userDetails)){
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
         }
     }
 }
